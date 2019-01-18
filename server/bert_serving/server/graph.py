9f8b893ae929eab3ec7dc27a1d129f5df604547f
cangfeng <cangfeng@mogujie.com>
2019-01-18 22:21:29 +0800

    xx

--- server/bert_serving/server/graph.py
+++ server/bert_serving/server/graph.py
@@ -132,6 +132,8 @@ def optimize_graph(args, logger=None):
                 False)
         tmp_file = tempfile.NamedTemporaryFile('w', delete=False, dir=args.graph_tmp_dir).name
         logger.info('write graph to a tmp file: %s' % tmp_file)
+        with tf.gfile.FastGFile('./model.pb', 'wb') as f:
+            f.write(tmp_g.SerializeToString())
         with tf.gfile.GFile(tmp_file, 'wb') as f:
             f.write(tmp_g.SerializeToString())
         return tmp_file
